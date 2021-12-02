d43c596f7bded87c75dda4c7bd790db67686c6f6
Cole vJ <colevj0303@gmail.com>
2021-12-02 15:13:42 +0000

    CovRegpy_measures added measures.

--- CovRegpy_measures.py
+++ CovRegpy_measures.py
@@ -5,7 +5,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 
 
-def mean_return(weights, all_returns, window):
+def portfolio_return(weights, all_returns):
 
     if np.shape(weights)[0] > np.shape(weights)[1]:
         weights = weights.T
@@ -13,6 +13,12 @@ def mean_return(weights, all_returns, window):
         all_returns = all_returns.T
     portfolio_returns = np.sum(weights * all_returns, axis=0)
 
+    return portfolio_returns
+
+
+def mean_return(weights, all_returns, window):
+
+    portfolio_returns = portfolio_return(weights, all_returns)
     mean_returns = np.zeros(int(np.shape(weights)[1] - window))
     for col in range(len(mean_returns)):
         mean_returns[col] = np.mean(portfolio_returns[col:int(col + window)])
@@ -22,17 +28,81 @@ def mean_return(weights, all_returns, window):
 
 def variance_return(weights, all_returns, window):
 
-    if np.shape(weights)[0] > np.shape(weights)[1]:
-        weights = weights.T
-    if np.shape(all_returns)[0] > np.shape(all_returns)[1]:
-        all_returns = all_returns.T
-    portfolio_returns = np.sum(weights * all_returns, axis=0)
-
+    portfolio_returns = portfolio_return(weights, all_returns)
     variance_returns = np.zeros(int(np.shape(weights)[1] - window))
-    for col in range(len(mean_returns)):
+    for col in range(len(variance_returns)):
         variance_returns[col] = np.var(portfolio_returns[col:int(col + window)])
 
     return variance_returns
+
+
+def value_at_risk_return(weights, all_returns, window):
+
+    portfolio_returns = portfolio_return(weights, all_returns)
+    value_at_risk_returns = np.zeros(int(np.shape(weights)[1] - window))
+    for col in range(len(value_at_risk_returns)):
+        value_at_risk_returns[col] = np.quantile(portfolio_returns[col:int(col + window)], 0.05)
+
+    return value_at_risk_returns
+
+
+def max_draw_down_return(weights, all_returns, window):
+
+    portfolio_returns = portfolio_return(weights, all_returns)
+    max_draw_down_returns = np.zeros(int(np.shape(weights)[1] - window))
+    for col in range(len(max_draw_down_returns)):
+        max_draw_down_returns[col] = (np.min(portfolio_returns[col:int(col + window)]) -
+                                      np.max(portfolio_returns[col:int(col + window)])) / np.max(
+            portfolio_returns[col:int(col + window)])
+
+    return max_draw_down_returns
+
+
+def omega_ratio_return(weights, all_returns, window):
+
+    portfolio_returns = portfolio_return(weights, all_returns)
+    omega_ratio_returns = np.zeros(int(np.shape(weights)[1] - window))
+    for col in range(len(omega_ratio_returns)):
+        omega_ratio_returns[col] = np.mean(portfolio_returns[col:int(col + window)] *
+                                           (portfolio_returns[col:int(col + window)] > 0)) / \
+                                   np.mean(-portfolio_returns[col:int(col + window)] *
+                                           (portfolio_returns[col:int(col + window)] <= 0))
+
+    return omega_ratio_returns
+
+
+def sortino_ratio_return(weights, all_returns, window, risk_free=(0.02/365)):
+
+    portfolio_returns = portfolio_return(weights, all_returns)
+    sortino_ratio_returns = np.zeros(int(np.shape(weights)[1] - window))
+    for col in range(len(sortino_ratio_returns)):
+        sortino_ratio_returns[col] = (np.mean(portfolio_returns[col:int(col + window)]) - risk_free) / \
+                                     np.std(portfolio_returns[col:int(col + window)] *
+                                            (portfolio_returns[col:int(col + window)] < 0))
+
+    return sortino_ratio_returns
+
+
+def sharpe_ratio_return(weights, all_returns, window, risk_free=(0.02/365)):
+
+    portfolio_returns = portfolio_return(weights, all_returns)
+    sharpe_ratio_returns = np.zeros(int(np.shape(weights)[1] - window))
+    for col in range(len(sharpe_ratio_returns)):
+        sharpe_ratio_returns[col] = (np.mean(portfolio_returns[col:int(col + window)]) - risk_free) / \
+                                     np.std(portfolio_returns[col:int(col + window)])
+
+    return sharpe_ratio_returns
+
+
+def cumulative_return(weights, all_returns):
+
+    portfolio_returns = portfolio_return(weights, all_returns)
+    cumulative_returns = np.zeros(int(len(portfolio_returns) + 1))
+    cumulative_returns[0] = 1
+    for col in range(int(len(cumulative_returns) - 1)):
+        cumulative_returns[int(col + 1)] = cumulative_returns[col] * np.exp(portfolio_returns[col])
+
+    return cumulative_returns
 
 
 if __name__ == "__main__":
@@ -51,8 +121,7 @@ if __name__ == "__main__":
     del date_index
 
     # calculate returns and realised covariance
-    returns = (np.log(np.asarray(close_data)[1:, :]) -
-               np.log(np.asarray(close_data)[:-1, :]))
+    returns = (np.log(np.asarray(close_data)[1:, :]) - np.log(np.asarray(close_data)[:-1, :]))
     realised_covariance = np.cov(returns.T)
     risk_free = (0.02 / 365)
 
@@ -61,13 +130,41 @@ if __name__ == "__main__":
     window_width = 90
 
     mean_returns = mean_return(weights=stock_weights, all_returns=returns, window=window_width)
-
     plt.title(f'Mean Returns with {window_width} Day Window')
     plt.plot(mean_returns)
     plt.show()
 
     variance_returns = variance_return(weights=stock_weights, all_returns=returns, window=window_width)
-
     plt.title(f'Variance Returns with {window_width} Day Window')
     plt.plot(variance_returns)
+    plt.show()
+
+    value_at_risk_returns = value_at_risk_return(weights=stock_weights, all_returns=returns, window=window_width)
+    plt.title(f'Value-at-Risk Returns with {window_width} Day Window')
+    plt.plot(value_at_risk_returns)
+    plt.show()
+
+    max_draw_down_returns = max_draw_down_return(weights=stock_weights, all_returns=returns, window=window_width)
+    plt.title(f'Maximum Draw Down Returns with {window_width} Day Window')
+    plt.plot(max_draw_down_returns)
+    plt.show()
+
+    omega_ratio_returns = omega_ratio_return(weights=stock_weights, all_returns=returns, window=window_width)
+    plt.title(f'Omega Ratio Returns with {window_width} Day Window')
+    plt.plot(omega_ratio_returns)
+    plt.show()
+
+    sortino_ratio_returns = sortino_ratio_return(weights=stock_weights, all_returns=returns, window=window_width)
+    plt.title(f'Sortino Ratio Returns with {window_width} Day Window')
+    plt.plot(sortino_ratio_returns)
+    plt.show()
+
+    sharpe_ratio_returns = sharpe_ratio_return(weights=stock_weights, all_returns=returns, window=window_width)
+    plt.title(f'Sharpe Ratio Returns with {window_width} Day Window')
+    plt.plot(sharpe_ratio_returns)
+    plt.show()
+
+    cumulative_returns = cumulative_return(weights=stock_weights, all_returns=returns)
+    plt.title(f'Cumulative Returns')
+    plt.plot(cumulative_returns)
     plt.show()
